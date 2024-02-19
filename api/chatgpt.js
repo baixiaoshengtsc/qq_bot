9451695743cc63208e902a28fb66790c3bef6ac4
baixiaoshengtsc <485434766@qq.com>
2024-02-20 01:37:49 +0800

    新增gpt

--- api/chatgpt.js
+++ api/chatgpt.js
@@ -1,17 +1,23 @@
 /*
  * @Author: baixiaoshengtsc 485434766@qq.com
- * @Date: 2024-02-17 10:52:38
+ * @Date: 2024-02-19 21:38:02
  * @LastEditors: baixiaoshengtsc 485434766@qq.com
- * @LastEditTime: 2024-02-20 00:55:46
- * @FilePath: \qq_bot\api\gpt3.5.js
+ * @LastEditTime: 2024-02-20 01:17:42
+ * @FilePath: \qq_bot\api\chatgpt.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const { apiList } = require('./');
-exports.getChat = async (input, sys, cache) => {
+const modelList = {
+  gpt4Plus: 'gpt-4-0125-preview',
+  gpt4Img: 'gpt-4-1106-vision-preview',
+  gpt4: 'gpt-4-1106-preview',
+  gpt3_5:'gpt-3.5-turbo-0125',
+}
+exports.chatgpt = async (input, sys, cache, temperature=0.7, type='gpt4') => {
   let arr
 
   if (input) {
-    if (cache.length > 20) {
+    if (cache.length > 24) {
       cache.shift()
     }
     cache.push(input)
@@ -29,18 +35,20 @@ exports.getChat = async (input, sys, cache) => {
   }
   try {
     let data = {
-      model: "gpt-4-1106-preview", 
-      messages: arr
+      model: modelList[type], 
+      messages: arr,
+      temperature: temperature
     };
     let completion = await apiList.getChat(data).then((response)=>{
-      console.log('---response---' ,response)
+      // console.log('---response---' ,response)
       return response.data.choices[0].message
     }).catch((error)=>{
-      console.log(error)
+      // console.log(error)
+      throw new Error(error);
     })
-
+    
     return completion
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 }
--- app_.js
+++ app_.js
@@ -10,6 +10,20 @@ const updateMessage = require('./middle/updateMessage')
 const {
   getChat
 } = require('./api/gpt.js')
+const {chatgpt} = require('./api/chatgpt.js')
+app.all("*",function(req,res,next){
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin","http://localhost:3000");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers","content-type, Authorization");
+  //跨域允许的请求方式 
+  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method.toLowerCase() == 'options')
+      res.send(200);  //让options尝试请求快速结束
+  else
+      next();
+})
 
 app.use(express.static('./'))
 app.use(express.json())
@@ -25,7 +39,38 @@ console.log(req.body)
   let str = (ret && ret.content) ? ret.content : `gpt并不想回答你的问题并试图用错误代码塞爆我的四核8g腾讯云`
   res.send(str)
 })
+app.post('/api/chat', async(req, res ,next) => {
+  console.log('--收到客户端消息--', req.body)
+  const option = req.body.option
+  const history = req.body.history
+  const content = req.body.content
+  
+  try {
+    const ret = await chatgpt({
+      role: 'user',
+      content
+    }, option.system, history.map(item => {
+      return {
+        role: item.type,
+        content: item.content
+      }
+    }), option.temperature, option.type)
+    res.send({
+      status: 0,
+      statusText: 'success',
+      data: ret
+    })
+  } catch (error) {
+    res.send({
+      status: -1,
+      statusText: error,
+      data: null
+    })
+  }
+  
 
+  
+})
 app.listen(8060, () => {
   console.log('server running ai http://127.0.0.1')
 }) 

168305426a613d47490636a7bc10c26bee8a0c17
baixiaoshengtsc <485434766@qq.com>
2024-03-15 20:35:40 +0800

    🐳 chore(新增docker):

--- common/index.js
+++ common/index.js
@@ -5,17 +5,27 @@ const key = process.env.API_KEY
 const AJAX = axios.create({
   baseUrl: '',
 })
-
+const getProxy = () => {
+  if(process.env.ENV === 'development') {
+    return {
+      host: "127.0.0.1",
+      port: 7890,
+      protocol: "http"
+    }
+  }else if(process.env.ENV === 'production') {
+    return {
+      host: "clash-docker",
+      port: 7890,
+      protocol: "http"
+    }
+  }
+}
 AJAX.interceptors.request.use(function (config) {
   Object.assign(config.headers, {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${key}`
   })
-  config.proxy = {
-    host: "127.0.0.1",
-    port: 7890,
-    protocol: "http"
-  }
+  config.proxy = getProxy()
   return config
 }, function(error){
   console.log('AJAX错误', error)

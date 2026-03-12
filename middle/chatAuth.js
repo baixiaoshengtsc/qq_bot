b214bc9964f2b0902e9a023928eb7d68584e7e89
baixiaoshengtsc <485434766@qq.com>
2026-03-12 16:09:21 +0800

    尝试修复

--- middle/chatAuth.js
+++ middle/chatAuth.js
@@ -19,9 +19,21 @@ module.exports = async (req, res, next) => {
     return
   }
   if(pw == '76b00f07-e408-1dd2-a8c6-7ce0800d8694') {
-    next()
+    const ret = await chatPwDAO.queryChatPw({pw: 'fefdc625-13a8-4a81-b82a-b0bf768eb5a1'})
+    if(!ret || ret.length<=0) {
+      res.send({
+        status: -1,
+        statusText : '卡密已失效或过期，请联系作者485434766',
+        data: null
+      })
+      return
+    }else {
+      req.nums = ret[0].nums
+      next()
+    }
+    // next()
   }else {
-    const ret = await chatPwDAO.queryChatPw({pw})
+    const ret = await chatPwDAO.queryChatPw({pw: 'fefdc625-13a8-4a81-b82a-b0bf768eb5a1'})
     if(!ret || ret.length<=0) {
       res.send({
         status: -1,
